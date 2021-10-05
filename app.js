9273c31d6ad9c96ed2be0aef97ad083c9669f63a
ShawnFrost26 <sb262202@gmail.com>
2021-10-05 16:52:49 +0530

    null vs undefined

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
-// string
-console.log("hello world")
-console.log(typeof 'd')
 
-// numbers
-console.log(1)
-console.log(typeof 5265825)
-console.log(typeof -6991)
-console.log(typeof 1478.2648)
 
-// boolean
-console.log(true)
-console.log(false)
-console.log(typeof false)
+
+
+
+var age = null
+console.log(typeof age)
+
+
