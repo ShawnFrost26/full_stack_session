2197d88f1e798d9ee90f17f49524d33d99244bd4
ShawnFrost26 <sb262202@gmail.com>
2021-10-05 16:35:37 +0530

    primitive data types

---
+++ app.js
@@ -0,0 +1,14 @@
+// string
+console.log("hello world")
+console.log(typeof 'd')
+
+// numbers
+console.log(1)
+console.log(typeof 5265825)
+console.log(typeof -6991)
+console.log(typeof 1478.2648)
+
+// boolean
+console.log(true)
+console.log(false)
+console.log(typeof false)
